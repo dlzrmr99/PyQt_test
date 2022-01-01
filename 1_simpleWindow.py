322ef60ee5c0cb67558bc29d8f2739b756bfcb67
10104 <dlzrmr99@gmail.com>
2022-01-01 17:34:18 +0100

    First Attempt.

--- 1_simpleWindow.py
+++ 1_simpleWindow.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     w = QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
+    w.resize(500, 650)
+    w.move(600, 600)
     w.setWindowTitle('Simple')
     w.show()
 
